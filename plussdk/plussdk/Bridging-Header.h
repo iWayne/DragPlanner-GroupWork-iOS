9044f68a2c283c65470cbedb1b4d9eeac989a85d
mewhuan <mew37112003@hotmail.com>
2015-04-20 17:18:38 -0400

    moving things

---
+++ plussdk/plussdk/Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  plussdk
+//
+//  Created by Dong Li on 4/3/15.
+//  Copyright (c) 2015 Dong Li. All rights reserved.
+//
+
+#ifndef plussdk_Bridge_Header_h
+#define plussdk_Bridge_Header_h
+#import <GooglePlus/GooglePlus.h>
+#import <GoogleOpenSource/GoogleOpenSource.h>
+#endif
---
+++ plussdk/plussdk/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  plussdk
+//
+//  Created by Dong Li on 4/9/15.
+//  Copyright (c) 2015 Dong Li. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+}
