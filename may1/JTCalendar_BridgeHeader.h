c41ce057f886b7b1c2c7ff7d93b5e933f703a605
mewhuan <mew37112003@hotmail.com>
2015-05-01 18:42:14 -0400

    parse
    with add and modify

---
+++ may1/JTCalendar_BridgeHeader.h
@@ -0,0 +1,15 @@
+//
+//  JTCalendar_BridgeHeader.h
+//  CalendarSw
+//
+//  Created by Shan Shawn on 4/6/15.
+//  Copyright (c) 2015 Shan Shawn. All rights reserved.
+//
+
+#import <JTCalendar/JTCalendar.h>
+
+#import "MADayView.h"
+#import "MAEvent.h"
+#import "MAEventKitDataSource.h"
+#import "NHCalendarActivity.h"
+//#import "JTCalendar.h"
