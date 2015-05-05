d77a97bb2d691b5ed0abbf84c840c757c2603c48
mewhuan <mew37112003@hotmail.com>
2015-05-04 22:51:47 -0400

    may4 last one

---
+++ may4evening2/CalendarSw/NotificationCenterFunc.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationCenterFunc.swift
+//  CalendarSw
+//
+//  Created by Shan Shawn on 5/3/15.
+//  Copyright (c) 2015 Shan Shawn. All rights reserved.
+//
+
+import Foundation
+
+//At the beginning of the Event
+func addNotiWithoutAction(maEvent:MAEvent){
+    var notification:UILocalNotification = UILocalNotification()
+    notification.alertBody = "Event: \(maEvent.title)"
+    notification.fireDate = maEvent.start
+    notification.userInfo = maEvent.userInfo
+    UIApplication.sharedApplication().scheduleLocalNotification(notification)
+}
+
+//At the end of the Event
+func addNotiWithAction(maEvent:MAEvent){
+    var notification:UILocalNotification = UILocalNotification()
+    notification.alertBody = "Event: \(maEvent.title)"
+    notification.category = "FIRST_CATEGORY"
+    notification.fireDate = maEvent.start
+    notification.userInfo = maEvent.userInfo
+    UIApplication.sharedApplication().scheduleLocalNotification(notification)
+}
+
+func cancalNoti(maEvent:MAEvent){
+    var eventInfo = maEvent.userInfo
+    for noti in UIApplication.sharedApplication().scheduledLocalNotifications {
+        //println(eventInfo)
+        if((noti as UILocalNotification).userInfo!["key"] as NSString == eventInfo["key"] as NSString){
+            UIApplication.sharedApplication().cancelLocalNotification(noti as UILocalNotification)
+        }
+    }
+}
