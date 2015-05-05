1ea3e7d2e1107c35be070e8b8072ed6d449c45e3
mewhuan <mew37112003@hotmail.com>
2015-05-04 22:27:46 -0400

    even

---
+++ may4evening/CalendarSw/GlobalFunc.swift
@@ -0,0 +1,34 @@
+//
+//  GlobalFunc.swift
+//  CalendarSw
+//
+//  Created by Shan Shawn on 5/4/15.
+//  Copyright (c) 2015 Shan Shawn. All rights reserved.
+//
+
+import Foundation
+
+func getMidnight(newDate:NSDate)->NSDate{
+    var calendar:NSCalendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)!
+    var comp:NSDateComponents = calendar.components(.CalendarUnitDay | .CalendarUnitMonth | .CalendarUnitYear, fromDate: newDate)
+    return calendar.dateFromComponents(comp)!
+}
+
+func identifyColor(colorCode: String) -> UIColor {
+    
+    if colorCode == "UIDeviceRGBColorSpace 0.898039 0.607843 0.607843 0.7" {
+        return bColor
+    }
+    else if colorCode == "UIDeviceRGBColorSpace 0.94902 0.756863 0.0941176 0.7" {
+        return rColor
+    }
+    else if colorCode == "UIDeviceRGBColorSpace 0.556863 0.788235 0.737255 0.7" {
+        return gColor
+    }
+    else if colorCode == "UIDeviceRGBColorSpace 0 1 0 1" {
+        return finishColor
+    }
+    else {
+        return finishColor
+    }
+}
