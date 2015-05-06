bc2df45c41fb860567b6fabf7db7e3f14a0d58bf
ishawn <ishawnshan@gmail.com>
2015-05-05 22:33:54 -0400

    Swift 1.2

---
+++ Drag Planner_Swift 1.2/Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,20 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JTCalendar
+#define COCOAPODS_POD_AVAILABLE_JTCalendar
+#define COCOAPODS_VERSION_MAJOR_JTCalendar 1
+#define COCOAPODS_VERSION_MINOR_JTCalendar 2
+#define COCOAPODS_VERSION_PATCH_JTCalendar 3
+
+// NHCalendarActivity
+#define COCOAPODS_POD_AVAILABLE_NHCalendarActivity
+#define COCOAPODS_VERSION_MAJOR_NHCalendarActivity 0
+#define COCOAPODS_VERSION_MINOR_NHCalendarActivity 0
+#define COCOAPODS_VERSION_PATCH_NHCalendarActivity 3
+
